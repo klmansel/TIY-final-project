427120f29a7ca7780e583aec4d5c7afe82e88ba9
Kara Mansel <callmedevmaybe@gmail.com>
2016-07-24 17:38:56 -0400

    attempting to adjust responsiveness

--- app/scripts/components/jets-site.jsx
+++ app/scripts/components/jets-site.jsx
@@ -42,7 +42,7 @@ var JetsPage = React.createClass({
             </div>
           </nav>
         </div>
-        <div className="row col-md-12">
+        <div className="row col-md-12 mobile-nav">
           <div className="new-nav col-xs-12 col-md-10 col-md-offset-1">
               <a className="col-xs-12 col-md-3 new-navlist" href="#">Photos</a>
               <a className="col-xs-12 col-md-3 new-navlist" href="#">Events</a>
